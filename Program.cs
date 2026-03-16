a83f6757b51a0030ea9cd4832980b844bec69ccd
Tymofii <s32813@pjwstk.edu.pl>
2026-03-16 22:46:44 +0100

    Add realisation of methods

--- Program.cs
+++ Program.cs
@@ -1,11 +1,28 @@
 ﻿Console.WriteLine("Program started");
 Console.WriteLine("Enter number:");
-if (int.TryParse(Console.ReadLine(), out int result)) {
+int number = Convert.ToInt32(Console.ReadLine());
+if (number, out int result)) {
     Console.WriteLine($"You inserted: {result}");
 } else {
     Console.WriteLine("Error! It's not a number!");
 }
-
+Console.WriteLine("Enter number:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number2, out int result)) {
+    Console.WriteLine($"You inserted: {result}");
+} else {
+    Console.WriteLine("Error! It's not a number!");
+}
+int number3 = Convert.ToInt32(Console.ReadLine());
+if (number3, out int result)) {
+    Console.WriteLine($"You inserted: {result}");
+} else {
+    Console.WriteLine("Error! It's not a number!");
+}
+int[] numbers = new[] { number, number2, number3 };
+Console.WriteLine("Average: "+CalculateAverage(numbers));
+Console.WriteLine("Min: "+CalculateMin(numbers));
+Console.WriteLine("Max: "+CalculateMax(numbers));
 public static double CalculateAverage(int[] values)
 {
     return values.Average();
